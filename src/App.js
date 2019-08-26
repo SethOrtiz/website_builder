0d1e4eb44c58eada927705660b4451a3dc538567
Seth Ortiz <sethl.ortiz@gmail.com>
2019-08-26 11:29:37 -0700

    setup router and paths

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import TopNav from './views/theme/Navbar'
+import Landing from './views/landing/Landing'
+import Dashboard from './views/dashboard/Dashboard'
+import Workstation from './views/workstation/Workstation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <TopNav />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/dashboard/:userId" component={Dashboard} />
+          <Route path="/workstation/:userId/:websiteId" component={Workstation} />
+          <Route path="*" render={() => <div>Not found</div>} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
