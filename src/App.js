903bcceeccb0e90deabeda566a3f4154c5ad24a5
Seth Ortiz <sethl.ortiz@gmail.com>
2019-08-26 15:06:44 -0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import TopNav from './views/theme/Navbar'
+import Navbar from './views/theme/Navbar'
 import Landing from './views/landing/Landing'
 import Dashboard from './views/dashboard/Dashboard'
 import Workstation from './views/workstation/Workstation'
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
     <Router>
       <div>
-        <TopNav />
+        <Navbar />
         <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/dashboard/:userId" component={Dashboard} />
-          <Route path="/workstation/:userId/:websiteId" component={Workstation} />
-          <Route path="*" render={() => <div>Not found</div>} />
+          <Route exact path={ROUTES.LANDING} component={Landing} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route path={ROUTES.WORKSTATION}component={Workstation} />
+          <Route path={ROUTES.NOT_FOUND} render={() => <div>Not found</div>} />
         </Switch>
       </div>
     </Router>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,3 +3,4 @@ export const SIGN_IN = '/signin';
 export const DASHBOARD = '/account';
 export const WORKSTATION = '/workstation'
 export const PASSWORD_FORGET = '/pw-forget';
+export const NOT_FOUND = '*';
