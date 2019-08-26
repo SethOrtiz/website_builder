7fa9ab07f374a9f93681cf3b9807a87680fa1e34
Seth Ortiz <sethl.ortiz@gmail.com>
2019-08-26 11:52:41 -0700

    configured section actions

---
+++ src/redux/sections/actions.js
@@ -0,0 +1,48 @@
+////////////// Sections Fetch /////////////
+
+export const LOADING_SECTIONS = "LOADING_SECTIONS";
+export function loadingSections() {
+  return {
+    type: LOADING_SECTIONS
+  };
+}
+
+export const SECTIONS_FETCH_ERROR = "SECTIONS_FETCH_ERROR";
+export function sectionsFetchError() {
+  return {
+    type: SECTIONS_FETCH_ERROR
+  };
+}
+
+export const RECEIVED_SECTIONS = "RECEIVED_SECTIONS";
+export function fetchSections(sections) {
+    return {
+    type: RECEIVED_SECTIONS,
+    payload: sections
+  };
+}
+
+
+///////////////////// Add Section ////////////////////////
+
+export const POSTING_SECTION = "POSTING_SECTION";
+export function postingSection() {
+    return {
+    type: POSTING_SECTION
+  };
+}
+
+export const SECTION_FAILED_TO_POST = "POST_FAILED_TO_SECTION";
+export function sectionFailedToPost() {
+  return {
+    type: SECTION_FAILED_TO_POST
+  };
+}
+
+export const SECTION_POST_SUCCESS = "SECTION_POST_SUCCES";
+export function sectionPostSuccess(newSectionJson) {
+    return {
+    type: SECTION_POST_SUCCESS,
+    payload: newSectionJson
+  };
+}
