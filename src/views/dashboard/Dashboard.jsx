2648040c63907538dc7b2103d1f168271e2833de
Seth Ortiz <sethl.ortiz@gmail.com>
2019-10-28 21:32:17 -0700

    fixing styling

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -4,35 +4,22 @@ import NotepadContainer from "../../redux/dashboard/notes/NotepadContainer";
 import MyWebsitesContainer from "../../redux/dashboard/websites/MyWebsitesContainer";
 import NavContainer from "../../redux/landing/nav/NavContainer";
 import Footer from "../Footer";
-/* COLOR SCHEME */
-/* blue: #d2e7ff  | babyblue: #e3f0ff  | white : #fffef9  | peach: #ffefd7  | lightpeach: #fff6e9 | salmon: "#e3c9c9"*/
-const styles = {
-  dashboard: {
-    backgroundColor: "#222",
-  },
-  aside: {
-    display: "grid",
-    backgroundColor: "#222"
-  },
-  main: {
-    backgroundColor: "#222"
-  },
-};
+
 
 export default function Dashboard() {
   return (
     <>
-      <NavContainer styles={styles.nav} />
-      <div style={styles.dashboard} className="layout">
-        <aside style={styles.aside}>
+      <div  className="layout">
+        <NavContainer  />
+        <aside >
           <ProfileContainer />
           <NotepadContainer />
         </aside>
-        <main style={styles.main}>
+        <main >
           <MyWebsitesContainer />
         </main>
       </div>
-      <Footer style={styles.footer} />
+      <Footer />
     </>
   );
 }
